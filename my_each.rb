76fab98961685149f3e86e6031ead1b041804abe
Vincent Tran <vincentuong@yahoo.com>
2021-05-19 03:28:42 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+  i = 0
+  while i < arr.length
+    yield(arr[i])
+    i += 1
+  end
+  arr
 end
 
+# arr = [1,2,3,4]
 
-
-def my_each(collection)
+# my_each(arr) do |i|
+#   puts "#{i}"
+# end
   
